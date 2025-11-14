9fe497974bd505368a0728563ea10b07e9b78357
ShahmeerAli <shahmeer.devel@gmail.com>
2025-11-15 00:45:36 +0500

    added page viewer

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,52 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<Map<String,dynamic>> items=[
+    {'color':Colors.pink,"image":"assets/images/Astra.png","name":"Astra","category":"Controller"},
+    {'color':Colors.green,"image":"assets/images/brimstone.png","name":"Brimstone","category":"Controller"},
+    {'color':Colors.yellow,"image":"assets/images/Clove.png","name":"Clove","category":"Controller"},
+    {'color':Colors.blue,"image":"assets/images/cypher.png","name":"Cypher","category":"Sentinel"},
+    {'color':Colors.orange,"image":"assets/images/Harbor.png","name":"Harbor","category":"Controller"},
+    {'color':Colors.purple,"image":"assets/images/iso.png","name":"Iso","category":"Duelist"},
+    {'color':Colors.pink,"image":"assets/images/jett.png","name":"Jett","category":"Duelist"},
+    {'color':Colors.green,"image":"assets/images/neon.png","name":"Neon","category":"Duelist"},
+    {'color':Colors.yellow,"image":"assets/images/Omen.png","name":"Omen","category":"Controller"},
+    {'color':Colors.red,"image":"assets/images/phoenix.png","name":"Phoenix","category":"Duelist"},
+    {'color':Colors.purple,"image":"assets/images/raze.png","name":"Raze","category":"Duelist"},
+    {'color':Colors.pink,"image":"assets/images/reyna.png","name":"Reyna","category":"Duelist"},
+    {'color':Colors.orange,"image":"assets/images/skye.png","name":"Skye","category":"Initiator"},
+    {'color':Colors.yellow,"image":"assets/images/veto.png","name":"Veto","category":"Sentinel"},
+    {'color':Colors.purple,"image":"assets/images/Viper.png","name":"Viper","category":"Controller"},
+    {'color':Colors.red,"image":"assets/images/vyse.png","name":"Vyse","category":"Sentinel"},
+    {'color':Colors.blue,"image":"assets/images/waylay.png","name":"Waylay","category":"Duelist"},
+    {'color':Colors.green,"image":"assets/images/yoru.png","name":"Yoru","category":"Duelist"}
+
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
+        child:PageView.builder(itemCount: items.length,itemBuilder: (context,index){
+          final list=items[index];
+              return SafeArea(
+                child: Container(
+                  color: list['color'],
+                  child: Column(
+                    children: [
+                      Text(list['name'],style: TextStyle(fontSize: 60,fontFamily: 'PermanentMarker',color: Colors.white),),
+                      Text(list['category'],style: TextStyle(fontFamily: 'PermanentMarker',fontSize: 35,color: Colors.white),),
+                      SizedBox(height: 90,),
+                      Image.asset(list['image'],width: 400,height: 400,),
 
-          ],
-        ),
+                    ],
+                  ),
+                ),
+              );
+        },physics:  BouncingScrollPhysics(),)
       ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -13,29 +13,7 @@ class SplashScreen extends StatefulWidget{
 }
 
 class SplashState extends State<SplashScreen>{
-  List<Map<String,String>> items=[
-    {"image":"assets/images/Astra.png","name":"Astra","category":"Controller"},
-    {"image":"assets/images/brimstone.png","name":"Brimstone","category":"Controller"},
-    {"image":"assets/images/Clove.png","name":"Clove","category":"Controller"},
-    {"image":"assets/images/cypher.png","name":"Cypher","category":"Sentinel"},
-    {"image":"assets/images/Harbor.png","name":"Harbor","category":"Controller"},
-    {"image":"assets/images/iso.png","name":"Iso","category":"Duelist"},
-    {"image":"assets/images/jett.png","name":"Jett","category":"Duelist"},
-    {"image":"assets/images/neon.png","name":"Neon","category":"Duelist"},
-    {"image":"assets/images/Omen.png","name":"Omen","category":"Controller"},
-    {"image":"assets/images/phoenix.png","name":"Phoenix","category":"Duelist"},
-    {"image":"assets/images/raze.png","name":"Raze","category":"Duelist"},
-    {"image":"assets/images/reyna.png","name":"Reyna","category":"Duelist"},
-    {"image":"assets/images/skye.png","name":"Skye","category":"Initiator"},
-    {"image":"assets/images/veto.png","name":"Veto","category":"Sentinel"},
-    {"image":"assets/images/Viper.png","name":"Viper","category":"Controller"},
-    {"image":"assets/images/vyse.png","name":"Vyse","category":"Sentinel"},
-    {"image":"assets/images/waylay.png","name":"Waylay","category":"Duelist"},
-    {"image":"assets/images/yoru.png","name":"Yoru","category":"Duelist"}
 
-
-
-  ];
 
   @override
   void initState() {
@@ -60,7 +38,8 @@ class SplashState extends State<SplashScreen>{
          mainAxisAlignment: MainAxisAlignment.center,
          crossAxisAlignment: CrossAxisAlignment.center,
          children: [
-           Lottie.asset("assets/raw/starting.json",width: 400,height: 400)
+           Lottie.asset("assets/raw/starting.json",width: 400,height: 400),
+           Text("Animation Turf",style: TextStyle(fontSize: 25,fontFamily: "PermanentMarker"),)
          ],
        ),
      ),
